54c13455ecaad781527a60ce80e30ca52992a53f
Stephen Schools <stephen.schools>
2017-08-02 15:15:59 -0400

    initial markup and button data array created but not complete

---
+++ main.js
@@ -0,0 +1,25 @@
+let buttonData = [
+  {"name": "clear"},
+  {"name": "power"},
+  {"name": "sqaure"},
+  {"name": "sqrt"},
+  {"name": "mod"},
+  {"name": "seven"},
+  {"name": "eight"},
+  {"name": "nine"},
+  {"name": "divide"},
+  {"name": "four"},
+  {"name": "five"},
+  {"name": "six"},
+  {"name": "multiply"},
+  {"name": "one"},
+  {"name": "two"},
+  {"name": "three"},
+  {"name": "subtract"},
+  {"name": "zero"},
+  {"name": "decimal"},
+  {"name": "equals"},
+  {"name": "add"}
+];
+
+console.log(buttonData.length);
