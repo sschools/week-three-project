adda9e7acef9b378f51694ae7484699f59d41d2e
Stephen Schools <stephen.schools>
2017-08-02 15:24:01 -0400

    a little more data added

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
 let buttonData = [
-  {"name": "clear"},
-  {"name": "power"},
-  {"name": "sqaure"},
-  {"name": "sqrt"},
-  {"name": "mod"},
-  {"name": "seven"},
-  {"name": "eight"},
-  {"name": "nine"},
-  {"name": "divide"},
-  {"name": "four"},
-  {"name": "five"},
-  {"name": "six"},
-  {"name": "multiply"},
-  {"name": "one"},
-  {"name": "two"},
-  {"name": "three"},
-  {"name": "subtract"},
-  {"name": "zero"},
-  {"name": "decimal"},
-  {"name": "equals"},
-  {"name": "add"}
+  {"name": "clear", "label": "C"},
+  {"name": "power", "label": "xy"},
+  {"name": "sqaure", "label": "x2"},
+  {"name": "sqrt", "label": "sqrt"},
+  {"name": "mod", "label": "mod"},
+  {"name": "seven", "label": "7"},
+  {"name": "eight", "label": "8"},
+  {"name": "nine", "label": "9"},
+  {"name": "divide", "label": "/"},
+  {"name": "four", "label": "4"},
+  {"name": "five", "label": "5"},
+  {"name": "six", "label": "6"},
+  {"name": "multiply", "label": "x"},
+  {"name": "one", "label": "1"},
+  {"name": "two", "label": "2"},
+  {"name": "three", "label": "3"},
+  {"name": "subtract", "label": "-"},
+  {"name": "zero", "label": "0"},
+  {"name": "decimal", "label": "."},
+  {"name": "equals", "label": "="},
+  {"name": "add", "label": "+"}
 ];
 
 console.log(buttonData.length);
+for (let i = 0; i < buttonData.length; i++) {
+  console.log(buttonData[i].name + " " + buttonData[i].label);
+}
