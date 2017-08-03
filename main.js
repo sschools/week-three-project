425cf5a440b23be0262f64a9715f12240e5555df
Stephen Schools <stephen.schools>
2017-08-03 12:49:23 -0400

    event listener established and identifying correct div, need functionality

--- main.js
+++ main.js
@@ -1,35 +1,43 @@
-const buttonData = [
-  {"name": "clear", "label": "C"},
-  {"name": "display", "label": "0"},
-  {"name": "power", "label": "xy"},
-  {"name": "sqaure", "label": "x2"},
-  {"name": "sqrt", "label": "sqrt"},
-  {"name": "mod", "label": "mod"},
-  {"name": "seven", "label": "7"},
-  {"name": "eight", "label": "8"},
-  {"name": "nine", "label": "9"},
-  {"name": "divide", "label": "/"},
-  {"name": "four", "label": "4"},
-  {"name": "five", "label": "5"},
-  {"name": "six", "label": "6"},
-  {"name": "multiply", "label": "x"},
-  {"name": "one", "label": "1"},
-  {"name": "two", "label": "2"},
-  {"name": "three", "label": "3"},
-  {"name": "subtract", "label": "-"},
-  {"name": "zero", "label": "0"},
-  {"name": "decimal", "label": "."},
-  {"name": "equals", "label": "="},
-  {"name": "add", "label": "+"}
-];
 
-console.log(buttonData.length);
-for (let i = 0; i < buttonData.length; i++) {
-  console.log(buttonData[i].name + " " + buttonData[i].label);
-}
+
+const buttonData = [
+  {"name": "clear", "label": "C", "class": "operator"},
+  {"name": "display", "label": ""},
+  {"name": "power", "label": "xy", "class": "operator"},
+  {"name": "sqaure", "label": "x2", "class": "operator"},
+  {"name": "sqrt", "label": "sqrt", "class": "operator"},
+  {"name": "mod", "label": "mod", "class": "operator"},
+  {"name": "seven", "label": "7", "class": "number", "value": 7},
+  {"name": "eight", "label": "8", "class": "number", "value": 8},
+  {"name": "nine", "label": "9", "class": "number", "value": 9},
+  {"name": "divide", "label": "/", "class": "operator"},
+  {"name": "four", "label": "4", "class": "number", "value": 4},
+  {"name": "five", "label": "5", "class": "number", "value": 5},
+  {"name": "six", "label": "6", "class": "number", "value": 6},
+  {"name": "multiply", "label": "x", "class": "operator"},
+  {"name": "one", "label": "1", "class": "number", "value": 1},
+  {"name": "two", "label": "2", "class": "number", "value": 2},
+  {"name": "three", "label": "3", "class": "number", "value": 3},
+  {"name": "subtract", "label": "-", "class": "operator"},
+  {"name": "zero", "label": "0", "class": "number", "value": 0},
+  {"name": "decimal", "label": "."},
+  {"name": "equals", "label": "=", "class": "operator"},
+  {"name": "add", "label": "+", "class": "operator"}
+];
 
 let container = document.getElementById("calculator-body");
 
+function clickEvent() {
+  let divClicked;
+  if (event.target.getAttribute("class")) {
+    divClicked = event.target;
+  } else {
+    divClicked = event.target.parentElement;
+  }
+  console.log(divClicked);
+}
+
+//loop to set up physical calulcator and assign appropriate classes
 for (let i = 0; i < buttonData.length; i++) {
   let newDiv = document.createElement("div");
   let newText = document.createElement("h2");
@@ -43,6 +51,11 @@ for (let i = 0; i < buttonData.length; i++) {
   if (i >= buttonData.length - 4) {
     newDiv.classList.add("bottom-row");
   }
+
+  if (buttonData[i].class) {
+    newDiv.classList.add(buttonData[i].class);
+  }
+  newDiv.addEventListener("click", clickEvent);
 
   newText.appendChild(content);
   newDiv.appendChild(newText);
