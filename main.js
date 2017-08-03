747670a8c86cb1a476a656bb261e1cd91d2d48a7
Stephen Schools <stephen.schools>
2017-08-03 13:41:11 -0400

    number buttons are functional and display

--- main.js
+++ main.js
@@ -34,7 +34,49 @@ function clickEvent() {
   } else {
     divClicked = event.target.parentElement;
   }
-  console.log(divClicked);
+  let divId = divClicked.id;
+  let num; //this will become the number that was clicked
+
+  switch(divId) {
+    case "nine":
+      num = 9;
+      break;
+    case "eight":
+      num = 8;
+      break;
+    case "seven":
+      num = 7;
+      break;
+    case "six":
+      num = 6;
+      break;
+    case "five":
+      num = 5;
+      break;
+    case "four":
+      num = 4;
+      break;
+    case "three":
+      num = 3;
+      break;
+    case "two":
+      num = 2;
+      break;
+    case "one":
+      num = 1;
+      break;
+    case "zero":
+      num = 0;
+      break;
+  }
+
+  let displayNum;
+  displayNum = num;
+
+  let dispArea = document.querySelector("#display h2");
+  let displayContent = document.createTextNode(displayNum);
+
+  dispArea.appendChild(displayContent);
 }
 
 //loop to set up physical calulcator and assign appropriate classes
@@ -55,6 +97,12 @@ for (let i = 0; i < buttonData.length; i++) {
   if (buttonData[i].class) {
     newDiv.classList.add(buttonData[i].class);
   }
+
+  let newClass = newDiv.getAttribute("class");
+  if (newClass.includes("number")) {
+    newDiv.setAttribute("value", buttonData[i].value);
+  }
+
   newDiv.addEventListener("click", clickEvent);
 
   newText.appendChild(content);
